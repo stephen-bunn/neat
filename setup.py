60fb6e5d79ea2460a6e2e33a82362dd9cebe8c81
Stephen Bunn <ritashugisha@gmail.com>
2017-04-17 02:12:57 -0400

    Fixing setup script

--- setup.py
+++ setup.py
@@ -4,16 +4,40 @@
 # Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
 # GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+from neat import const
+
 
 setup(
-    name='NEAT',
-    version='0.0.0',
+    name=const.module_name,
+    version=const.version,
     description='Networked Energy Appliance Translator',
+    long_description='README.md',
+    license='GNU GPLv3',
     author='Stephen Bunn',
     author_email='stephen@bunn.io',
+    url='https://github.com/ritashugisha/neat',
+    platforms='Unix',
+    include_package_data=True,
+    install_requires=[
+        'sphinx>=1.5.2',
+        'nose>=1.3.7',
+        'coverage>=4.3.4',
+        'jsonschema>=2.6.0',
+        'requests>=2.13.0',
+        'requests-mock>=1.3.0',
+        'blinker>=1.4',
+        'pint>=0.7.2',
+        'lxml>=3.4.2',
+        'beautifulsoup4>=4.5.3',
+        'python-dateutil>=2.6.0',
+        'pyyaml>=3.12',
+        'rethinkdb>=2.3.0.post6',
+        'pymongo>=3.4.0'
+    ],
     packages=[
-        'neat',
-        'neat.requester', 'neat.scheduler', 'neat.translator'
+        'neat', 'neat.models', 'neat.requester', 'neat.scheduler',
+        'neat.translator', 'neat.pipe'
     ]
 )
